51aaad5b2c7550a28c854bdb4b235b8c5a533793
JP19-03 <u202014511@upc.edu.pe>
2024-11-28 22:40:33 -0500

    feat: add connection to mongodb database.

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
-
+/** Authentication and Registration routes */
+router.post('/auth/register', (req, res) => {
+    console.log(req.body);
+});
 
 export default router;
