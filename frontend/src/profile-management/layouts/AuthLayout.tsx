ec18cee786f24efbb110b8f50dfb2ee891b83cea
JP19-03 <u202014511@upc.edu.pe>
2024-11-30 22:27:31 -0500

    feat(frontend): add form, validation for register user.

--- frontend/src/profile-management/layouts/AuthLayout.tsx
+++ frontend/src/profile-management/layouts/AuthLayout.tsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
 
 function AuthLayout() {
-    return (  
+  return (
     <>
-        <div className="bg-slate-800 min-h-screen">
-            <div className="max-w-lg mx-auto pt-10 px-5">
-                <img src="/logo.svg" alt= "Logo DevTree" />
+      <div className="bg-slate-800 min-h-screen">
+        <div className="max-w-lg mx-auto pt-10 px-5">
+          <img src="/logo.svg" alt="Logo DevTree" />
 
-                <div className="py-10">
-                    <Outlet />
-                </div>
-            </div>
+          <div className="py-10">
+            <Outlet />
+          </div>
         </div>
+      </div>
     </>
-    );
+  );
 }
 
 export default AuthLayout;
--- frontend/src/profile-management/pages/RegisterPage.tsx
+++ frontend/src/profile-management/pages/RegisterPage.tsx
@@ -1,18 +1,146 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import type { UserRegister } from "../models/User";
+import ErrorMessage from "../components/ErrorMessage";
 
 function RegisterPage() {
-    return (  
-    <>
-        <h1 className="text-4xl text-white font-bold">Create an account</h1>
+  const initialValues = {
+    name: "",
+    email: "",
+    handle: "",
+    password: "",
+    password_confirmation: "",
+  };
 
-        <nav className="mt-10">
-            <Link 
-                className="text-center text-white text-lg block"
-                to="/auth/login"
-            >Already have an account? Login here</Link>
-        </nav>
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserRegister>({ defaultValues: initialValues });
+
+  const handleRegister = (formData: UserRegister) => {
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <h1 className="text-4xl text-white font-bold">Create an account</h1>
+
+      <form
+        onSubmit={handleSubmit(handleRegister)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="name" className="text-2xl text-slate-500">
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Your Name"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="handle" className="text-2xl text-slate-500">
+            Handle
+          </label>
+          <input
+            id="handle"
+            type="text"
+            placeholder="Username: no spaces"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("handle", {
+              required: "Handle is required",
+            })}
+          />
+          {errors.handle && (
+            <ErrorMessage>{errors.handle.message}</ErrorMessage>
+          )}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <div className="grid grid-cols-1 space-y-3">
+          <label
+            htmlFor="password_confirmation"
+            className="text-2xl text-slate-500"
+          >
+            Repeat Password
+          </label>
+          <input
+            id="password_confirmation"
+            type="password"
+            placeholder="Repeat Password"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("password_confirmation", {
+              required: "Password confirmation is required",
+              validate: (value) =>
+                value === watch("password") || "Passwords do not match",
+            })}
+          />
+          {errors.password_confirmation && (
+            <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Register"
+        />
+      </form>
+
+      <nav className="mt-10">
+        <Link className="text-center text-white text-lg block" to="/auth/login">
+          Already have an account? Login here
+        </Link>
+      </nav>
     </>
-    );
+  );
 }
 
 export default RegisterPage;
