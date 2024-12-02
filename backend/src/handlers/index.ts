9a23b2fcfc581483488ee95f9223ca993fd2624b
JP19-03 <u202014511@upc.edu.pe>
2024-12-01 22:16:23 -0500

    feat(backend): implement JWT generation and improve error handling in account creation and login

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -2,27 +2,28 @@ import type { Request, Response } from "express";
 import slug from "slug";
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
+import { generateJWT } from "../utils/jwt";
 
-export const createAccount = async (req: Request, res: Response) => {  
+export const createAccount = async (req: Request, res: Response) => {
 
-    const {email, password} = req.body;
-    
-    const userExists = await User.findOne({email});
+    const { email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
     if (userExists) {
         const error = new Error("User with this email already exists");
-        res.status(409).json({error: error.message});
+        res.status(409).json({ error: error.message });
         return;
     }
 
     const handle = slug(req.body.handle, '');
-    const handleExists = await User.findOne({handle});
+    const handleExists = await User.findOne({ handle });
     if (handleExists) {
         const error = new Error("User with this handle already exists");
-        res.status(409).json({error: error.message});
+        res.status(409).json({ error: error.message });
         return;
     }
 
-    const user = new User(req.body); 
+    const user = new User(req.body);
     user.password = await hashPassword(password);
     user.handle = handle;
 
@@ -33,13 +34,13 @@ export const createAccount = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     // Check if user exists
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     if (!user) {
         const error = new Error("User does not exist");
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
         return;
     }
 
@@ -47,9 +48,11 @@ export const login = async (req: Request, res: Response) => {
     const isPasswordCorrect = await checkPassword(password, user.password);
     if (!isPasswordCorrect) {
         const error = new Error("Invalid password");
-        res.status(401).json({error: error.message});
+        res.status(401).json({ error: error.message });
         return;
     }
 
-    res.send("Login successful");
+    const token = generateJWT({ id: user._id });
+
+    res.send(token);
 }
