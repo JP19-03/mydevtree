ddfcfd083a8ac362bfbd0b9bad03c8807a3e6c46
JP19-03 <u202014511@upc.edu.pe>
2024-12-03 12:57:18 -0500

    feat(frontend): implement user profile form with validation and error handling.

--- frontend/src/profile-management/pages/ProfilePage.tsx
+++ frontend/src/profile-management/pages/ProfilePage.tsx
@@ -1,6 +1,28 @@
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../components/ErrorMessage";
+
 function ProfilePage() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      handle: "",
+      description: "",
+    },
+  });
+
+  const handleUserProfileForm = () => {
+    console.log("Form submitted");
+  };
+
   return (
-    <form className="bg-white p-10 rounded-lg space-y-5" onSubmit={() => {}}>
+    <form
+      className="bg-white p-10 rounded-lg space-y-5"
+      onSubmit={handleSubmit(handleUserProfileForm)}
+    >
       <legend className="text-2xl text-slate-800 text-center">
         Edit Profile
       </legend>
@@ -10,7 +32,12 @@ function ProfilePage() {
           type="text"
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="handle or username"
+          {...register("handle", {
+            required: "Handle is required",
+          })}
         />
+
+        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
@@ -18,7 +45,14 @@ function ProfilePage() {
         <textarea
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="Your description here"
+          {...register("description", {
+            required: "Description is required",
+          })}
         />
+
+        {errors.description && (
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
+        )}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
