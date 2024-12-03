ed3acbc613b2eb3e5194cecea6d2e822c85c00a8
JP19-03 <u202014511@upc.edu.pe>
2024-12-03 13:22:52 -0500

    feat(backend): add updateProfile handler for user profile updates with validation.

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -60,3 +60,28 @@ export const login = async (req: Request, res: Response) => {
 export const getUser = async (req: Request, res: Response) => {
     res.json(req.user);
 }
+
+export const updateProfile = async (req: Request, res: Response) => {
+    try {
+        const { description } = req.body;
+
+        const handle = slug(req.body.handle, '');
+        const handleExists = await User.findOne({ handle });
+        if (handleExists && handleExists.email !== req.user.email) {
+            const error = new Error("User with this handle already exists");
+            res.status(409).json({ error: error.message });
+            return;
+        }
+
+        // Update user
+        req.user.description = description;
+        req.user.handle = handle;
+        await req.user.save();
+        res.send("Profile updated successfully");
+
+    } catch (e) {
+        const error = new Error("Failed to update profile");
+        res.status(500).json({ error: error.message });
+        return;
+    }
+}
