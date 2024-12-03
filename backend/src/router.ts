0bfb63541bf328ecccd57cbae6526d249a788ab4
JP19-03 <u202014511@upc.edu.pe>
2024-12-03 13:22:58 -0500

    feat(backend): add updateProfile route with validation for user profile updates.

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body } from "express-validator"
-import { createAccount, getUser, login } from "./handlers";
+import { createAccount, getUser, login, updateProfile } from "./handlers";
 import { handleInputErrors } from "./middleware/validation";
 import { authenticate } from "./middleware/auth";
 
@@ -35,6 +35,18 @@ router.post('/auth/login',
     login
 );
 
-router.get('/user', authenticate, getUser)
+router.get('/user', authenticate, getUser);
+
+router.patch('/user',
+    body('handle')
+        .notEmpty()
+        .withMessage('Handle is required'),
+    body('description')
+        .notEmpty()
+        .withMessage('Description is required'),
+    handleInputErrors,
+    authenticate,
+    updateProfile
+);
 
 export default router;
