8695c546e61fad515b8c4c4dd2f3a45e9d5ab62f
JP19-03 <u202014511@upc.edu.pe>
2024-12-02 19:42:16 -0500

    feat(frontend): implement user data fetching with React Query and add request interceptor for auth token.

--- frontend/src/application-management/layouts/AppLayout.tsx
+++ frontend/src/application-management/layouts/AppLayout.tsx
@@ -1,8 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useQuery } from "@tanstack/react-query";
 import NavigationTabs from "../components/NavigationTabs";
+import { getUser } from "../../profile-management/services/user.service";
 
 export default function AppLayout() {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: getUser,
+    queryKey: ["user"],
+    retry: 1,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(data);
+
   return (
     <>
       <header className="bg-slate-800 py-5">
