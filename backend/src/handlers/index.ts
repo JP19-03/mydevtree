7149839cad177e767c3f9e68086bb3442fc0af43
JP19-03 <u202014511@upc.edu.pe>
2024-12-04 20:18:49 -0500

    feat(backend): implement image upload handler using formidable and Cloudinary

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,8 +1,11 @@
 import type { Request, Response } from "express";
 import slug from "slug";
+import formidable from "formidable";
+import { v4 as uuid } from "uuid";
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
 import { generateJWT } from "../utils/jwt";
+import cloudinary from "../config/cloudinary";
 
 export const createAccount = async (req: Request, res: Response) => {
 
@@ -81,6 +84,31 @@ export const updateProfile = async (req: Request, res: Response) => {
 
     } catch (e) {
         const error = new Error("Failed to update profile");
+        res.status(500).json({ error: error.message });
+        return;
+    }
+}
+
+export const uploadImage = async (req: Request, res: Response) => {
+    const form = formidable({ multiples: false });
+
+    try {
+        form.parse(req, (error, fields, files) => {
+            cloudinary.uploader.upload(files.file[0].filepath, { public_id: uuid() }, async function (error, result) {
+                if (error) {
+                    const error = new Error("Failed to upload image");
+                    res.status(500).json({ error: error.message });
+                    return;
+                }
+                if (result) {
+                    req.user.image = result.secure_url;
+                    await req.user.save();
+                    res.json({ image: result.secure_url });
+                }
+            })
+        });
+    } catch (e) {
+        const error = new Error("Failed to upload image");
         res.status(500).json({ error: error.message });
         return;
     }
