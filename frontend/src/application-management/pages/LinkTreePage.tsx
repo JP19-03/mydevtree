ffe9b24ee1d1100c22247e186fb2f6bea70f6a45
JP19-03 <u202014511@upc.edu.pe>
2024-12-05 22:44:00 -0500

    feat(frontend): implement LinkTreePage for managing social media links with validation

--- frontend/src/application-management/pages/LinkTreePage.tsx
+++ frontend/src/application-management/pages/LinkTreePage.tsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../../utils";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateProfile } from "../../profile-management/services/user.service";
+import { User } from "../../profile-management/models/User";
+
 function LinkTreePage() {
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+  const queryClient = useQueryClient();
+
+  const user: User = queryClient.getQueryData(["user"])!;
+
+  const { mutate } = useMutation({
+    mutationFn: updateProfile,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {
+      toast.success("Links updated successfully");
+    },
+  });
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    );
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error("Invalid URL");
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+
+    queryClient.setQueryData(["user"], (prevData: User) => {
+      return {
+        ...prevData,
+        links: JSON.stringify(updatedLinks),
+      };
+    });
+  };
+
   return (
-    <div>
-      <h1>LinkTreePage</h1>
+    <div className="space-y-5">
+      {devTreeLinks.map((item) => (
+        <DevTreeInput
+          key={item.name}
+          item={item}
+          handleUrlChange={handleUrlChange}
+          handleEnableLink={handleEnableLink}
+        />
+      ))}
+      <button
+        className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg         font-bold"
+        onClick={() => mutate(user)}
+      >
+        Save Changes
+      </button>
     </div>
   );
 }
